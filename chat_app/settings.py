ff4941e4825d3ff1c71990426a9177adcd7a1553
devesh aggrawal <deveshaggrawal19@gmail.com>
2017-05-26 21:46:18 +0530

    adding requirement.txt file and changing the settings for postgresql database

--- chat_app/settings.py
+++ chat_app/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'chat_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'chat_database',
+	'USER': 'chat_user',
+	'PASSWORD': 'chat_password',
+	'HOST': 'localhost',
+	'PORT': '',
     }
 }
 
